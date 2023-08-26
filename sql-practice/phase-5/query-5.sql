c9d9bdd188fbe794efbbcbc28a394df78979e26a
Tim Mairose <tmairose@gmail.com>
2023-08-26 14:56:28 -0600

    phase5 done?

--- sql-practice/phase-5/query-5.sql
+++ sql-practice/phase-5/query-5.sql
@@ -1,2 +1,33 @@
 -- Find names of the cats whose owners are both George Beatty and Melynda Abshire, or just George Beatty, or just Melynda Abshire
 -- Your code here
+SELECT cats.name
+FROM cats
+JOIN cat_owners
+ON cats.id = cat_owners.cat_id
+JOIN owners
+ON owners.id = cat_owners.owner_id
+WHERE owners.first_name = 'George'
+AND owners.last_name = 'Beatty'
+UNION
+SELECT cats.name
+FROM cats
+JOIN cat_owners
+ON cats.id = cat_owners.cat_id
+JOIN owners
+ON owners.id = cat_owners.owner_id
+WHERE owners.first_name = 'Melynda'
+UNION
+SELECT cats.name
+FROM cats
+JOIN cat_owners
+ON cats.id = cat_owners.cat_id
+JOIN owners
+ON owners.id = cat_owners.owner_id
+WHERE owners.first_name = 'Melynda'
+OR owners.first_name = 'George';
+-- AND owners.last_name = 'Abshire'
+-- AND owners.last_name = 'Beatty';
+-- WHERE (owners.first_name = 'George' AND owners.last_name = 'Beatty')
+--    OR (owners.first_name = 'Melynda' AND owners.last_name = 'Abshire')
+--    OR (owners.first_name = 'George' AND owners.last_name <> 'Beatty')
+--    OR (owners.first_name = 'Melynda' AND owners.last_name <> 'Abshire');
